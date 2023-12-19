d1b3d8b3b107c30cffbeec0fa1b618a9b2fa1f68
Douglas Finlay <douglas@douglasfinlay.com>
2023-12-19 22:09:30 +0000

    Fix failing tests

--- src/osc-argument-list-joiner.test.ts
+++ src/osc-argument-list-joiner.test.ts
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { EosOscMessage } from './eos-osc-stream';
+import { OscMessage } from './osc';
 import { OscArgumentListJoiner } from './osc-argument-list-joiner';
 
 describe('OSC argument list joiner', () => {
@@ -11,70 +11,66 @@ describe('OSC argument list joiner', () => {
         });
 
         it('Should pass through messages that do not use the argument list convention', () => {
-            const message: EosOscMessage = {
-                address: '/demo/message',
-                args: ['a', 'b', 'c', 1, 2, 3],
-            };
+            const message = new OscMessage('/demo/message', [
+                'a',
+                'b',
+                'c',
+                1,
+                2,
+                3,
+            ]);
 
             const result = joiner.process(message);
             assert.deepStrictEqual(result, message);
         });
 
         it('Should hold messages until all arguments have been collected', () => {
-            let result = joiner.process({
-                address: '/demo/message/list/0/6',
-                args: ['a', 'b'],
-            });
+            let result = joiner.process(
+                new OscMessage('/demo/message/list/0/6', ['a', 'b']),
+            );
             assert.isNull(result);
 
-            result = joiner.process({
-                address: '/demo/message/list/2/6',
-                args: ['c', 1],
-            });
+            result = joiner.process(
+                new OscMessage('/demo/message/list/2/6', ['c', 1]),
+            );
             assert.isNull(result);
 
-            result = joiner.process({
-                address: '/demo/message/list/4/6',
-                args: [2, 3],
-            });
-            assert.deepStrictEqual(result, {
-                address: '/demo/message',
-                args: ['a', 'b', 'c', 1, 2, 3],
-            });
+            result = joiner.process(
+                new OscMessage('/demo/message/list/4/6', [2, 3]),
+            );
+            assert.deepStrictEqual(
+                result,
+                new OscMessage('/demo/message', ['a', 'b', 'c', 1, 2, 3]),
+            );
         });
 
         it('Should throw an error if an out-of-sequence argument list is received', () => {
             assert.throws(() => {
-                joiner.process({
-                    address: '/demo/message/list/2/6',
-                    args: ['a', 'b'],
-                });
+                joiner.process(
+                    new OscMessage('/demo/message/list/2/6', ['a', 'b']),
+                );
             });
 
-            joiner.process({
-                address: '/demo/message/list/0/6',
-                args: ['a', 'b'],
-            });
+            joiner.process(
+                new OscMessage('/demo/message/list/0/6', ['a', 'b']),
+            );
 
             assert.throws(() => {
-                joiner.process({
-                    address: '/demo/message/list/4/6',
-                    args: [2, 3],
-                });
+                joiner.process(
+                    new OscMessage('/demo/message/list/4/6', [2, 3]),
+                );
             });
         });
 
         it('Should throw an error if an out-of-sequence message is received', () => {
-            joiner.process({
-                address: '/demo/message/list/0/6',
-                args: ['a', 'b'],
-            });
+            joiner.process(
+                new OscMessage('/demo/message/list/0/6', ['a', 'b']),
+            );
 
             assert.throws(() => {
-                joiner.process({
-                    address: '/demo/unexpected/message',
-                    args: ['a', 'b'],
-                });
+                joiner.process(
+                    new OscMessage('/demo/unexpected/message', ['a', 'b']),
+                );
             });
         });
     });
